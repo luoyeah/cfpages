cc8edd22bebeed7b0d63d53767a9d0ee1488c57f
None <None>
2025-10-23 20:24:11 +0800

    增加笔记

--- public/01我的笔记/data.js
+++ public/01我的笔记/data.js
@@ -1,10 +1,7 @@
 export const data = {
     files: [
         { id: 1, name: "Scoop安装教程", icon: "fa-book", file: "scoopapps.md" },
-        { id: 2, name: "测试", icon: "fa-rocket", file: "test.md" },
-        // { id: 3, name: "API文档", icon: "fa-code", file: "api.md" },
-        // { id: 4, name: "示例项目", icon: "fa-laptop-code", file: "examples.md" },
-        // { id: 5, name: "常见问题", icon: "fa-question-circle", file: "faq.md" }
+        { id: 2, name: "收藏网站", icon: "fa-book", file: "links.md" }
     ]
 };
 
---
+++ public/01我的笔记/md/links.md
@@ -0,0 +1,6 @@
+# 收藏网站
+
+
+## 游戏
+
+[围住小猫](https://ganlvtech.github.io/phaser-catch-the-cat/)
